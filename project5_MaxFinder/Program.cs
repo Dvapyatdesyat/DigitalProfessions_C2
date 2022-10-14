96dc759111de535a2e8e41553e7b2d416c350356
Max 2.50 <maximqlpin@mail.ru>
2022-10-14 11:26:40 +0500

    project 5

---
+++ project5_MaxFinder/Program.cs
@@ -0,0 +1,15 @@
+﻿int A=3;
+int B=10;
+int C=44;
+int D=2;
+int E=1;
+int Max = A;
+
+if (A>Max) Max = A;
+if (B>Max) Max = B;
+if (C>Max) Max = C;
+if (D>Max) Max = D;
+if (E>Max) Max = E;
+
+Console.Write("Max= ");
+Console.WriteLine(Max);
