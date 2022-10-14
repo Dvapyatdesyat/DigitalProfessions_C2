2d341468f7adff7b3a522a3c4a2f3d666b7adf1c
Max 2.50 <maximqlpin@mail.ru>
2022-10-14 10:56:09 +0500

    Project 3 Summator v1.0

--- project3_Summator/Program.cs
+++ project3_Summator/Program.cs
@@ -1,5 +1,8 @@
-﻿int NumberA=3;
-int NumberB=10;
-Console.Write(NumberA + NumberB);
+﻿int NumberA = new Random().Next(1, 10);
+Console.WriteLine(NumberA); 
+int NumberB = new Random().Next(1, 10);
+Console.WriteLine(NumberB); 
+int result = NumberA + NumberB;
+Console.Write(result);
 
 
