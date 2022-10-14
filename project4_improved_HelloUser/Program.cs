2046e2cb33bb1f1f18ca2ac6d11cba7f5f0654c1
Max 2.50 <maximqlpin@mail.ru>
2022-10-14 11:17:26 +0500

    Project 4 Improved HelloUser v1.0

---
+++ project4_improved_HelloUser/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Insert name of user: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Huraaa, it's Masha :-*");
+}
+else
+{
+       Console.Write("Hello, "); 
+       Console.WriteLine(username);
+}
