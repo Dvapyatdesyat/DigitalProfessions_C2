d4cc2b0d3c69bbf923ba6bbf0c1f751d37ac8ade
Max 2.50 <maximqlpin@mail.ru>
2022-10-16 12:51:01 +0500

    проверка квадрата числа

--- project_S1.1/Program.cs
+++ project_S1.1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// написать программу, которая на вход принимает два числа и проверяет является ли первое число квадратом второго
+
+
+Console.WriteLine("Программа проверки квадрата числа");
+// ввести числа
+Console.WriteLine("Введите число номер 1");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число номер 2");
+int number2 = int.Parse(Console.ReadLine()!);
+
+// проверить выполнение условие
+if (number1*number1 == number2)
+{
+    Console.WriteLine("Число 2 является квадратом числа 1");
+}
+else
+{
+    Console.WriteLine("Число 2 НЕ является квадратом числа 1");
+}
+
