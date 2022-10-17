127deec607b349662e593144455c568a21cdf2ec
Max 2.50 <maximqlpin@mail.ru>
2022-10-17 11:36:08 +0500

    обновление после лекции 2

---
+++ project_L2.2_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int[] array = { 55, 2, 1312, 9, 5, 234, 7, 8, 9 };
+int n = array.Length;
+
+int find = 9;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+        index++;
+}
+
+
+
