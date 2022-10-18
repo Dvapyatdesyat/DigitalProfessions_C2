6663c04cb0b75fbc191283baef6d20614ec81382
Max 2.50 <maximqlpin@mail.ru>
2022-10-18 10:02:25 +0500

    реструктуризация репозитория

---
+++ Lesson 1/project5_MaxFinder/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+
+int A = 3;
+int B = 10;
+int C = 44;
+int D = 2;
+int E = 1;
+int F = 3;
+int G = 95;
+int H = 8;
+int I = 13;
+
+
+int max = Max (Max (A, B, C), Max (D, E, F), Max (G, H, I));
+Console.Write("Max= ");
+Console.WriteLine(max);
+
+
+
---
+++ Seminar 2/project_S2.1_kratnost/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите Число №1: ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите Число №2: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+bool Kvadrat(int number1, int number2)
+{
+    return number2 == number1 * number1 || number1 == number2 * number2;
+}
+
+if (Kvadrat(number1,number2))
+{
+    Console.WriteLine("Одно из чисел является квадратом другого");
+}
+else
+{
+    Console.WriteLine("Числа не являются квадратами друг друга");
+}
