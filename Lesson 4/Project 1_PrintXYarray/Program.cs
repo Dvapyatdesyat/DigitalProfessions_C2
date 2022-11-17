aeba752dae0883d681369608be43cc12211fd3a7
Max 2.50 <maximqlpin@mail.ru>
2022-11-17 23:15:52 +0300

    Выполнено задание после лекции 4

---
+++ Lesson 4/Project 1_PrintXYarray/Program.cs
@@ -0,0 +1,42 @@
+﻿// string [,] table = new string [2,5];
+// table [1,2] = "word";
+
+// for (int rows=0; rows<2; rows++)
+// {
+//     for (int columns=0; columns<5; columns++)
+//     {
+//         Console.WriteLine ($"-{table[rows,columns]}-");
+//     }
+// }
+
+
+
+
+void Printarray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows, columns] = new Random().Next(1, 10);
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] table = new int[3, 4];
+Printarray(table);
+FillArray (table);
+Printarray(table);
